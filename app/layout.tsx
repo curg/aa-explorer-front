2519f0100784fdf1510ffdf0fb47ce12b1722919
Resister-boy <hassanpumped17@gmail.com>
2023-05-06 15:53:23 +0900

    chore: set recoil

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
+"use client"
+import { RecoilRoot } from 'recoil'
 import './globals.css'
 import { Inter } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <RecoilRoot>
+        <body className={inter.className}>{children}</body>
+      </RecoilRoot>
     </html>
   )
 }
