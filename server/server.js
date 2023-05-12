2a5d5a54a5e7eb5f7914e5be4c3b4715af70932b
scottXchoo <scott@a41.io>
2023-05-12 23:45:36 +0900

    feat: database.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
 import { ApolloServer, gql } from "apollo-server";
+import { blocks } from "./database.js";
 
 const typeDefs = gql`
+  type Block {
+    id: ID!
+    age: Int
+    txn: Int
+    hash: String!
+    value: Float
+  }
+
   type Query {
-    hello: String!
+    blocks: [Block]
   }
 `;
 
+const resolvers = {
+  Query: {
+    blocks() {
+      return blocks;
+    },
+  },
+};
+
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
 });
 
 server.listen().then(({ url }) => {
