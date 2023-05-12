ee05121aff068f50a650dd928d9b047720d590a2
scottXchoo <scott@a41.io>
2023-05-12 23:56:31 +0900

    feat: frontend mock-server

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { GET_BLOCKS } from "@/src/core/apollo/query";
+import { useQuery } from "@apollo/client";
+
 export default function Home() {
+  const { data, loading, error } = useQuery(GET_BLOCKS);
+
+  console.log("data", data.blocks);
+
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>error</div>;
+
   return <div>home</div>;
 }
