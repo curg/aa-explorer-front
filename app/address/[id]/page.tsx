375b516e7b3e54c1ec01d2685e8385627e924284
Resister-boy <hassanpumped17@gmail.com>
2023-05-17 00:18:15 +0900

    feat: add address page

---
+++ app/address/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from '@/components/etc/Head'
+
+const Page = () => {
+  return (
+    <>
+      <Head />
+    </>
+  )
+}
+
+export default Page
---
+++ app/address/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from '@/src/components/etc/Head';
+import SearchBar from '@/src/components/common/searchBar/SearchBar';
+
+const Page = () => {
+  return (
+    <>
+      <Head title='AA | Address' />
+      <main className='w-full h-screen bg-black'>
+        <SearchBar className='mx-auto mb-12 w-4/5' />
+        <div className='mx-auto min-h-fit	w-4/5 bg-primaryBg rounded-xl'>
+          <div className='text-white py-12 px-12 flex items-center'>
+            <h2 className='font-semibold text-xl'>Overview</h2>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Page
