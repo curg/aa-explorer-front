a6503006c26be4b9b5ab4216eeb7261b3dc04387
Resister-boy <hassanpumped17@gmail.com>
2023-05-17 01:24:42 +0900

    feat: add link tag on logo

--- src/components/common/layout/Logo.tsx
+++ src/components/common/layout/Logo.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <div className='text-white font-semibold text-2xl'>
+    <Link
+      href={`/`}
+      className='text-white font-semibold text-2xl'>
       AA Rounge
-    </div>
+    </Link>
   )
 }
 
